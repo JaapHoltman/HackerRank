93d70fe50450de6382db730dc9641bdbcca9434b
JaapHoltman <47182194+JaapHoltman@users.noreply.github.com>
2020-10-11 13:40:09 +0200

    java reflection uitleg

---
+++ src/com/jaapholtman/advanced/ReflectionExplained.java
@@ -0,0 +1,54 @@
+package com.jaapholtman.advanced;
+
+
+    /*JAVA reflection is a very powerful tool to inspect the attributes of a class in runtime.
+    For example, we can retrieve the list of public fields of a class using getDeclaredMethods().
+    In this problem, you will be given a class Solution in the editor. You have to fill in the
+    incompleted lines so that it prints all the methods of another class called Student in
+    alphabetical order. We will append your code with the Student class before running it.
+    There is no sample input/output for this problem. If you press "Run Code", it will compile
+     it, but it won't show any outputs. */
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
+    public class ReflectionExplained {
+
+        public static void main(String[] args) {
+            Class student = Student.class;
+            Method[] methods = student.getDeclaredMethods();
+
+            ArrayList<String> methodList = new ArrayList<>();
+            for (Method m : methods) {
+                methodList.add(m.getName());
+            }
+            Collections.sort(methodList);
+            for (String name : methodList) {
+                System.out.println(name);
+            }
+        }
+
+        class Student {
+            private String name;
+            private String id;
+            private String email;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+
+            public void setEmail(String email) {
+                this.email = email;
+            }
+        }
+    }
+
+
+
